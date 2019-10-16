72bcfc48ae252c00763c2d6741d66083a80bc98e
Yogesh Galav <mr.yogesh.galav@gmail.com>
2019-10-16 12:51:18 +0530

    Version1.1 student seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(CountryTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(StudentsTableSeeder::class);
         $this->call(CoursesTableSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ExplorePostSeeder::class);
