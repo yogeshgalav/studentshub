4be4cc49318a1d6f698eda417b3f8fb59239537b
Yogesh Galav <mr.yogesh.galav@gmail.com>
2019-10-28 17:02:03 +0530

    Version-1.1 webpack spelling correct

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/Guest/app.js', 'public/js/app.js')
-.js('resources/js/Seeker/app.js', 'public/js/seekerApp.js')
-.js('resources/js/Student/app.js', 'public/js/studentApp.js')
+mix.js('resources/js/guest/app.js', 'public/js/app.js')
+.js('resources/js/seeker/app.js', 'public/js/seekerApp.js')
+.js('resources/js/student/app.js', 'public/js/studentApp.js')
     .sass('resources/sass/app.scss', 'public/css') ;
     // mix.extend('foo',new class{
     //     webpackPlugins(){
