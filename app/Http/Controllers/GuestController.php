817d76c9a71018f2117d7de1d5c21d55e854422a
yogeshgalav <mr.yogesh.galav@gmail.com>
2020-01-22 22:28:49 +0530

    Version-1.2 mail working

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -6,17 +6,29 @@ use Illuminate\Http\Request;
 use App\Models\Guest;
 use App\Mails\SubscriptionFirstMail;
 use Mail;
+use Log;
 
 class GuestController extends Controller
 {
     //
     public function update(Request $request)
     {
+        $email=$request->input('email');
+        DB::beginTransaction();
+    try{
         $guest=Guest::where('ip',$request->ip())->first();
-        $guest->email=$request->input('email');
+        $guest->email=$email;
         $guest->is_subscribed=true;
         $guest->save();
 
-        Mail::to($request->input('email'))->send(new SubscriptionFirstMail());
+        Mail::to($email)->send(new SubscriptionFirstMail());
+        DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            Log::critical('user subscription failure: for email id#'.$email);
+            // dd($e->getMessage(),$e->getLine());
+            return response()->$e;
+        }
+        return 'success';
     }
 }
