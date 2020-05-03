7587f0d42f4d1bc18c7dcc1782fa0c5404ea8b64
yogeshgalav <mr.yogesh.galav@gmail.com>
2020-05-03 11:35:00 +0530

    Version-1.3 updated fillable property to course

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
-    protected  $guarded = ['id', 'created_at', 'updated_at'];
-
     public static function AuthUserCategory(){
         return \Auth::user()->student->prefferred_category;
     }
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     //
+    protected  $guarded = ['id', 'created_at', 'updated_at'];
+
     public function category(){
         return $this->belongsTo('App\Models\Category');
     }
