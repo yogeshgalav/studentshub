10ebd20810112d6c4df996b0bc2516412b5e300e
raj <Raj>
2020-09-04 11:21:07 +0530

    Attempted questions UI updated

--- resources/js/views/student-classroom/classroom-daily-assignment.vue
+++ resources/js/views/student-classroom/classroom-daily-assignment.vue
@@ -43,6 +43,83 @@
                     </div>
                 </div>
             </div>
+
+  <div class="card mt-3 mb-3  bg-default ">
+                <div class="card-header">
+                    <h3 class="text-center font-size-18 text-black">
+                        {{ 'Attempted Questions Status' }}
+                    </h3>
+                </div>
+                <div class="card-body bg-white border-bottom-left-8 border-bottom-right-8">
+                    <div class="row">
+                        <div class="col-md-12 col-12 center-col">
+                            
+
+             <div
+                    v-for="(question,index) in daily_assignment.daily_questions"
+                    :key="index"
+                    class="col-md-12"
+                  >
+                    <div class="row">
+                      <div class="col-md-12 mb-1 mt-3">
+                        <div class="row">
+                          <div class="col-md-12">
+                            <div class="weight-800">{{'Question' + (index+1)}} </div>
+                          </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+                   
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="row">
+                          <div class="col-md-12">
+                            <div class="mb-2">{{question.question_text}}</div>
+                          </div>
+                        </div>
+                        <div
+                          class="row"
+                          v-for="(choice,index2) in question.multiple_choice"
+                          :key="index2"
+                        >
+                       <div class="col-md-1" >
+                            <div class="form-check ml-3 ">
+                              <input
+                                class="form-check-input"
+                                type="radio"
+                                disabled
+                                :name="'answers['+index+'][answer]'"
+                                :id="'correctAnswer'+index2"
+                                :value="choice.option_order"
+                              />
+                            
+                            </div>
+                        </div>
+                       
+                          <div class="col-md-3">{{choice.option_text}}</div>
+                           <div class="col-md-3" v-if="choice.is_correct"><i class="fa fa-check text-success"></i> </div>
+                           <div class="col-md-3" v-else><i class="fa fa-times text-danger"></i> </div>
+                          
+                     
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="col-md-12">
+<p class="text-black mb-1"> Marks obtained : <span class="text-success weight-800"> {{ daily_report.marks_obtained }} </span> | Rank : <span class="text-success weight-800"> {{ daily_report.rank }} </span></p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
         <div v-if="daily_report===null && daily_assignment!==null">
             <div id="reflection-incomplete" class="card mt-3 mb-3 bg-primary border-primary">
@@ -71,6 +148,8 @@
                     </div>
                 </div>
             </div>
+
+
         </div>
     </div>
 </template>
@@ -86,6 +165,7 @@
             return {
                 daily_report: null,
                 daily_assignment: null,
+               
             };
         },
         mounted() {
@@ -93,6 +173,7 @@
                 resp) => {
                     this.daily_report = resp.data.success.daily_report;
                     this.daily_assignment = resp.data.success.daily_assignment;
+                   
                 })
         }
     }
