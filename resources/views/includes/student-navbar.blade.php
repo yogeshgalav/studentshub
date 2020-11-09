00bf89b72a1393b582584ae41a94d8702d25f023
raj <Raj>
2020-11-09 09:21:08 +0530

    Fixed the searchbar UI bug

--- resources/views/includes/student-navbar.blade.php
+++ resources/views/includes/student-navbar.blade.php
@@ -13,7 +13,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav menu_head dash_search">
-                <li class="nav-item active search_box ">
+                <li class="nav-item  search_box ">
                     @include('includes.search-form')
                 </li>
                 <li>
