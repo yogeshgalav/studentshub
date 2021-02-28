f415408067d0d0794f2bb96012c949ad8670b068
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-02-28 14:17:39 +0530

    join id field in register made visible

--- resources/js/views/auth/register.vue
+++ resources/js/views/auth/register.vue
@@ -51,13 +51,6 @@
                           name="_token"
                           :value="csrfToken"
                         >
-                        <input
-                          id="token"
-                          type="hidden"
-                          class="form-control"
-                          name="join_id"
-                          :value="join_id"
-                        >
                       </div>
                       <div class="form-group">
                         <label> {{ trans('Full Name') }} </label>
@@ -144,6 +137,29 @@
                             >
                           </div>
                           <span class="error">{{ errors.first('password_confirmation') }}</span>
+                        </div>
+                      </div>
+
+
+                      <div class="form-group">
+                        <label> {{ 'Join ID (optional)' }} </label>
+                        <div class="inner-addon left-addon">
+                          <div class="input_icon_frm">
+                            <span class="icon_design_input"><i
+                              class="fa fa-user"
+                            /></span>
+                            <input
+                              id="join_id"
+                              v-model="join_id"
+                              v-validate="'required'"
+                              type="text"
+                              class="form-control"
+                              name="join_id"
+                              placeholder="Classroom Join ID"
+                              autofocus
+                            >
+                          </div>
+                          <span class="error">{{ errors.first('join_id') }}</span>
                         </div>
                       </div>
 
@@ -270,7 +286,6 @@ export default {
 		};
 	},
 	mounted(){  
-		this.join_id = this.$route.query.joinId ? this.$route.query.joinId : '';
 		this.$validator.localize('en', this.dict);
 	},
 	methods: {
