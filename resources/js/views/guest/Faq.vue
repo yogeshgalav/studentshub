cb65d204ca38f4fd2a32889d1b49c7f17baa20c8
himanshusingh-coder <74043821+himanshusingh-coder@users.noreply.github.com>
2021-03-06 14:43:21 +0530

    faq

--- resources/js/views/guest/Faq.vue
+++ resources/js/views/guest/Faq.vue
@@ -18,67 +18,66 @@
         </h2>
       </h1>
     </div>
-
+    <div class="form justify-content-center">
+      <form
+        class="col-md-6"
+        @submit.prevent="handleSubmit"
+      >
+        <div
+          v-if="!AuthUser"
+          class="form-group row"
+        >
+          <div class="col-md-12">
+            <label
+              for="email"
+              class="col-form-label text-md-right"
+            >{{ "Email" }}</label>
+            <input
+              id="email"
+              v-model="email"
+              v-validate="'required|email'"
+              type="email"
+              class="form-control"
+              name="email"
+            >
+            <span class="text-danger"> {{ formErrors("email") }}</span>
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="col-md-12">
+            <label
+              for="query"
+              class="col-form-label text-md-right"
+            >
+              Query
+            </label>
+            <textarea
+              id="query"
+              v-model="query"
+              v-validate="'required'"
+              type="text"
+              rows="10"
+              class="form-control"
+              name="query"
+            />
+            <span class="text-danger"> {{ formErrors("query") }}</span>
+          </div>
+        </div>
+        <div class="form-group row mb-0">
+          <div class="col-md-8">
+            <button
+              type="submit"
+              class="btn btn-primary"
+            >
+              {{ "Submit" }}
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
     <div class="col-md-12">
       <div class="row justify-content-center">
         <div class="col-md-6 mt-100 p-2">
-          <div class="form justify-content-center">
-            <form
-              class="col-md-12"
-              @submit.prevent="handleSubmit"
-            >
-              <div
-                v-if="!AuthUser"
-                class="form-group row"
-              >
-                <div class="col-md-12">
-                  <label
-                    for="email"
-                    class="col-form-label text-md-right"
-                  >{{ "Email" }}</label>
-                  <input
-                    id="email"
-                    v-model="email"
-                    v-validate="'required|email'"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                  >
-                  <span class="text-danger"> {{ formErrors("email") }}</span>
-                </div>
-              </div>
-              <div class="form-group row">
-                <div class="col-md-12">
-                  <label
-                    for="query"
-                    class="col-form-label text-md-right"
-                  >
-                    Query
-                  </label>
-                  <textarea
-                    id="query"
-                    v-model="query"
-                    v-validate="'required'"
-                    type="text"
-                    rows="10"
-                    class="form-control"
-                    name="query"
-                  />
-                  <span class="text-danger"> {{ formErrors("query") }}</span>
-                </div>
-              </div>
-              <div class="form-group row mb-0">
-                <div class="col-md-8">
-                  <button
-                    type="submit"
-                    class="btn btn-primary"
-                  >
-                    {{ "Submit" }}
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">
