a58502d1038c42d23e40e779ac22827f246ff92d
Shivansh shukla <lordshivansh2303@gmail.com>
2021-03-08 13:35:10 +0530

    retrieving faqs from db and showing in vue

--- resources/js/views/guest/Faq.vue
+++ resources/js/views/guest/Faq.vue
@@ -76,52 +76,19 @@
     <div class="col-md-12">
       <div class="row justify-content-center">
         <div class="col-md-6 mt-100 p-2">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                <strong>Q.</strong> How to access portal
-              </h5>
-              <p class="card-text">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum."
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                <strong>Q.</strong> how to reschdule class
-              </h5>
-              <p class="card-text">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum."
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">
-                <strong>Q.</strong> How to get access to course
-              </h5>
-              <p class="card-text">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum."
-              </p>
+          <div
+            v-for="(faq, index) in faqs"
+            :key="index"
+          >
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">
+                  <strong>Q.</strong> {{ faq.query.trim() }}
+                </h5>
+                <p class="card-text">
+                  {{ faq.answer }}
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -130,25 +97,7 @@
     </div>
   </main>
 </template>
-<style scoped>
-.mt-100 {
-  margin-top: 150px;
-}
-strong {
-  font-size: 20px;
-}
-.card {
-  margin-top: 20px;
-}
-input {
-  border-radius: 0%;
-}
-.heading{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
+
 
 <script>
 import FormMixin from '../../components/mixins/form-mixin.js';
@@ -190,3 +139,23 @@ export default {
 	},
 };
 </script>
+
+<style scoped>
+.mt-100 {
+  margin-top: 150px;
+}
+strong {
+  font-size: 20px;
+}
+.card {
+  margin-top: 20px;
+}
+input {
+  border-radius: 0%;
+}
+.heading{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
