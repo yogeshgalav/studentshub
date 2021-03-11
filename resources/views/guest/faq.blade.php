ba25ae46f40932d47326f0f26ffd43e85b8b704c
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-03-11 16:10:29 +0530

    ST-3 blade component binding

--- resources/views/guest/faq.blade.php
+++ resources/views/guest/faq.blade.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-
-@include('includes.meta')
-@include('includes.title')
-
-<script src="{{ asset('js/app.js') }}" defer></script>
-    <link href="{{ asset('css/guest.css') }}" type="text/css" rel="stylesheet">
-@include('includes.fonts')
-<script src="/js/lang.js"></script>
-<link rel="shortcut icon" type="image/x-icon" href="{{asset('favicon1.ico')}}" />
-</head>
-<body>
-    <div id="app">
-        <main class="flex-center position-ref full-height">   
-<div class="main-header">
-    @include('includes.logo-navbar')
-</div>
-<faq-component :faqs={{ $faqs }} ></faq-component>
-</main>
-</div>
-    @include('includes.jsVariables')
-</body>
-</html>
+@extends('guest.app')
+@section('content')
+<faq-component :faqs="{{ json_encode($faqs) }}" ></faq-component>
+@endsection
