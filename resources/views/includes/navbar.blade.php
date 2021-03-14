0fbf8eccf2cbb760dd87604633eb6dff6bbc114c
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-03-15 02:36:26 +0530

    ST-15 student-attempt navbar changed to blank navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-@if(in_array(request()->path(),['reset-password','check-in']))
+@if(in_array(request()->path(),['reset-password','check-in']) || request()->is('*daily-attempt'))
 <div class="row">
     <div class="col-md-3 col-12">
         <a href="/">
