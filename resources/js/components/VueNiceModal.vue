c25060140ecba82f3e5d1d6d52daff5358ee8cad
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-04-18 02:00:36 +0530

    Question model

--- resources/js/components/VueNiceModal.vue
+++ resources/js/components/VueNiceModal.vue
@@ -2,7 +2,7 @@
   <div
     :id="name"
     :ref="name"
-    class="modal fade"
+    :class="['modal fade', size]"
     role="dialog"
     tabindex="-1"
     :aria-labelledby="name"
@@ -51,6 +51,10 @@ export default {
 		heading:{
 			default:'',
 			required:true,
+		},
+		size:{
+			default:'',
+			required:false,
 		},
 	},
 	data(){
