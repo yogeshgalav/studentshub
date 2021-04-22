59d7b7d3bfd828a366f5a79896bbc209467f138a
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-04-22 15:16:33 +0530

    Bug fixes

--- routes/api/guest.php
+++ routes/api/guest.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/subscribe','GuestController@update');
 Route::get('/get-post-content/{post_id}','PostController@show');
-Route::post('/login','AuthController@loginViaApi');
-Route::post('/register','AuthController@registerViaApi');
+Route::post('/login',[App\Http\Controllers\AuthController::class,'loginViaApi']);
+Route::post('/register',[App\Http\Controllers\AuthController::class,'registerViaApi']);
 Route::post('/member-request','GuestController@memberRequest');
 
-Route::post('/forgot-password','AuthController@processForgotPassword');
+Route::post('/forgot-password',[App\Http\Controllers\AuthController::class,'processForgotPassword']);
 Route::post('/reset-password',[App\Http\Controllers\AuthController::class,'resetPassword2'])->middleware('auth:api');
 Route::post('/reset-password/{token}',[App\Http\Controllers\AuthController::class,'resetPassword']);
 Route::post('/feedback','GuestController@feedback');
