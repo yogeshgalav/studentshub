88758afffe7e6e43d5a8aa87d84509610bf40c22
himanshusingh-coder <74043821+himanshusingh-coder@users.noreply.github.com>
2021-04-22 18:12:46 +0530

    start attempt

--- resources/js/views/student-classroom/daily-attempt.vue
+++ resources/js/views/student-classroom/daily-attempt.vue
@@ -5,7 +5,7 @@
         class="btn btn-primary btn-lg m-0-a"
         @click="startAttempt"
       >
-        Start Attempt
+        Start Attempt <i class="fas fa-arrow-right" />
       </button>
     </div>
     <div v-else>
