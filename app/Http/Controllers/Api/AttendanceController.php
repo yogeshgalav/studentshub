ea139d20445b1195c8de1d57ac2cd2828c033a86
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-04-23 14:38:51 +0530

    Bug fixes

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -77,7 +77,6 @@ class AttendanceController extends Controller
 
     public function getAttendanceData($classroomId, Request $request){
         $selected_date = $request->date ?? $this->currentTime->toDateString();
-        // dd($selected_date,)
         $today = DB::table('classroom_users as cu')->where('cu.classroom_id',$classroomId)
         ->join('users as us','us.id','=','cu.user_id')
         ->join('students as st','st.user_id','=','us.id')
