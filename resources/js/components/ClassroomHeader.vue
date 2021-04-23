a4a3fe8e8494daab6b2196c83370af8cc3e28409
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-04-23 15:57:53 +0530

    classroom header fix

--- resources/js/components/ClassroomHeader.vue
+++ resources/js/components/ClassroomHeader.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-md-12 custom-margin">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="">
+            <a
+              class="btn btn-link ml-2 mb-2 font-size-18"
+              @click="$router.back()"
+            >
+              <i
+                class="fa fa-arrow-left"
+                aria-hidden="true"
+              />
+              Back
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12">
         <h2 class="font-size-40 text-black weight-800 mb-2 line-height-25-px">
           {{ classroomDetail.name }}
         </h2>
