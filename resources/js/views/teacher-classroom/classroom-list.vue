0939725f7b49fe86d3a0ddf6b43121e9638d6109
webronak <webronak2030@gmail.com>
2021-04-24 10:56:33 +0530

    built classroom list card details

--- resources/js/views/teacher-classroom/classroom-list.vue
+++ resources/js/views/teacher-classroom/classroom-list.vue
@@ -136,27 +136,90 @@
     >
       <i class="fas fa-plus" />&nbsp;&nbsp;Join Classroom
     </button>
+
+    <div class="card mt-3 mb-3">
+      <div class="card-body">
+            <div class="card-header">
+                Details
+            </div>
+            <vue-table-component
+                :columns="classroomColumns"
+                :rows="classroomRows"
+            />
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
 import Modal from '../../components/VueNiceModal';
 import swal from '../../components/swal';
 import AddButton from '../../components/AddButton';
+import VueTableComponent from '../../../../resources/js/components/vue-table-component'
 
 export default {
 	components:{
 		Modal,
-		AddButton
+		AddButton,
+        VueTableComponent
 	},
 	props: ['myClassrooms', 'classroomList'],
 	data() {
 		return {
 			id_error: '',
 			showLoader: false,
-			join_classroom_name: ''
+			join_classroom_name: '',
+            classroomRows:[],
+            classroomColumns: [
+				{
+					label: 'Classroom',
+					field: 'classroom_name',
+				},
+				{
+					label: 'Teacher',
+					field: 'teacher',
+				},
+				{
+					label: 'Subject',
+					field: 'subject_name',
+				},
+                {
+					label: 'Students',
+					field: 'total_students',
+				},
+				{
+					label: 'Join Id',
+					field: 'join_id',
+				},
+                {
+					label: 'Daily Assignments',
+					field: 'total_daily_assignments',
+				},
+				{
+					label: 'Doubts',
+					field: 'total_doubts',
+				},
+                {
+					label: 'Resources',
+					field: 'total_resources',
+				},
+                {
+					label: 'Messages',
+					field: 'total_messages',
+				},
+                {
+					label: 'Avg. Score',
+					field: 'average_score',
+				}
+			]
 		};
 	},
+    mounted(){
+        this.axios.get('/api/classroom-list-details').then((resp)=>{
+            this.classroomRows=resp.data.success.classrooms
+        })
+    },
 	methods:{
 		joinClassroom(){
 			this.showLoader=true;
