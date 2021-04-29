ac43349af81c039c13a491789285b8d722a6a77d
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-04-29 08:28:03 +0530

    Version-1.6 css merged

--- resources/views/institute/app.blade.php
+++ resources/views/institute/app.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.institute')
 @section('compiledJs')
 <script src="{{ asset('js/instituteApp.js') }}" defer></script>
-<link href="{{ asset('css/instituteApp.css') }}" type="text/css" rel="stylesheet">
 @endsection
 @section('content')
 @yield('content')
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -5,6 +5,7 @@
 @include('includes.meta')
 @include('includes.title')
 @yield('compiledJs')
+<link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet">
 @include('includes.fonts')
 <script src="/js/lang.js"></script>
 <link rel="shortcut icon" type="image/x-icon" href="{{asset('favicon.ico')}}" />
