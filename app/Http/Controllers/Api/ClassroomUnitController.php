069077e9c75ceb39d6779f1292891bce0bcf5541
webronak <webronak2030@gmail.com>
2021-04-30 14:12:01 +0530

    worked on unit data value and fixed

--- app/Http/Controllers/Api/ClassroomUnitController.php
+++ app/Http/Controllers/Api/ClassroomUnitController.php
@@ -15,14 +15,32 @@ class ClassroomUnitController extends Controller
     //
     //api end point for getting unit assisment data for students and teachers
     public function getClassroomUnitDetails(Request $request){
-        $unitData=Unit::where('classroom_id',$request->classroomId)->get();
+        $unitData=Unit::where('classroom_id',$request->classroomId)
+        ->orderBy('units.unit_no','DESC')->get();
 
+        $daily_assignment_status = DB::table('daily_assignments')
+        ->where('classroom_id',$request->classroomId)
+        ->select(DB::raw('COUNT(distinct daily_assignments.id) as assignmentCount'),'daily_assignments.status','unit_id')
+        ->groupBy('status','unit_id')
+        ->get();
+
+        $summary = DB::table('classrooms as cl')
+        ->where('cl.id',$request->classroomId)
+        ->leftJoin('units', 'cl.id', '=', 'units.classroom_id')
+        ->leftJoin('daily_assignments', 'cl.id', '=', 'daily_assignments.classroom_id')
+        ->leftJoin('classroom_resources', 'cl.id', '=', 'classroom_resources.classroom_id')
+        ->leftJoin('daily_reports', 'daily_assignments.id', '=', 'daily_reports.daily_assignment_id')
+        ->select('units.id',DB::raw('COUNT(distinct daily_assignments.id) as assignmentCount'),DB::raw('AVG(daily_reports.marks_obtained) as averageScore'),DB::raw('COUNT(distinct classroom_resources.id) as resources'))
+        ->groupBy('units.id')
+        ->get();
         return response()->json([
             'success'=>[
-                'unitData'=>$unitData
+                'unitData'=>$unitData,
+                'summary'=>$summary,
+                'daily_assignment_status'=>$daily_assignment_status
             ]
         ]);
-    }   
+    }
     public function getUnitAssismentDetails(Request $request){
         $unitData=Unit::where('classroom_id',$request->classroomId)
         ->with('descriptiveQuestions')
@@ -33,7 +51,7 @@ class ClassroomUnitController extends Controller
                 'unitData'=>$unitData
             ]
         ]);
-    }   
+    }
 
     public function updateUnit($classroomId,Request $request){
         $unit = Unit::updateOrCreate([
