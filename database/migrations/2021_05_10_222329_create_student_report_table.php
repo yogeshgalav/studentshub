fcfe8cbb6c9861686899a752d296ef7ac2a3e477
jhonnysrinivas <mathasrinivas32@gmail.com>
2021-05-12 14:10:51 +0530

    srinivas/ST-53

--- database/migrations/2021_05_10_222329_create_student_report_table.php
+++ database/migrations/2021_05_10_222329_create_student_report_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateStudentReportTable extends Migration
 {
@@ -22,6 +23,12 @@ class CreateStudentReportTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('unit_id')->references('id')->on('units')->onDelete('cascade');
         });
+        $sql = "INSERT INTO `student_reports` (`user_id`, `unit_id`, `score_type`,`score`)
+        SELECT dr.user_id,da.unit_id,'first',dr.marks_obtained
+        FROM daily_reports as dr
+        LEFT JOIN daily_assignments as da
+        ON dr.daily_assignment_id=da.id";
+        DB::unprepared($sql);
     }
 
     /**
