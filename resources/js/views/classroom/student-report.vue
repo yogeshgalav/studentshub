7d27d5c59d2bdbcbb5df5574d6622e4973087571
Ronak singh <webronak2030@gmail.com>
2021-05-14 13:22:46 +0530

    initiate line graph

--- resources/js/views/classroom/student-report.vue
+++ resources/js/views/classroom/student-report.vue
@@ -2,62 +2,49 @@
   <div>
     <doughnut-graph :graph-data="pieChartData" />
     <multiBar-graph :graph-data="[{ id: 2, count: 3 }]" />
-    <linear-graph :graph-data="[{ id: 2, count: 3 }]" />
+    <dualLine-graph
+    v-if="dualLineChartData.length"
+      :line-one-data="dualLineChartData.map(node=>node.classroom_score)"
+      :line-two-data="dualLineChartData.map(node=>node.student_score)"
+      :labels="dualLineChartData.map(node=>node.attempt_date)"
+      line-one-label="Classroom score"
+      line-two-label="Student score"
+    />
   </div>
 </template>
 
 <script>
-import DoughnutGraph from "../../components/graphs/DoughnutGraph";
-import MultiBarGraph from "../../components/graphs/MultiBarGraph";
-import LinearGraph from "../../components/graphs/LinearGraph";
+import DoughnutGraph from '../../components/graphs/DoughnutGraph';
+import MultiBarGraph from '../../components/graphs/MultiBarGraph';
+import DualLineGraph from '../../components/graphs/DualLineGraph';
 
 export default {
-  components: {
-    DoughnutGraph,
-    MultiBarGraph,
-    LinearGraph,
-  },
-  data() {
-    return {
-      showLoader: true,
-      assignments_attemps: null,
-    };
-  },
-  computed: {
-    pieChartData() {
-      console.log(this.assignments_attemps);
-      return this.assignments_attemps;
-    },
-  },
-  mounted() {
-    let url =
-      "/api/classroom/" +
+	components: {
+		DoughnutGraph,
+		MultiBarGraph,
+		DualLineGraph,
+	},
+	data() {
+		return {
+			showLoader: true,
+			pieChartData: [],
+			dualLineChartData:[]
+		};
+	},
+	mounted() {
+		let url =
+      '/api/classroom/' +
       this.$route.params.classroomId +
-      "/get-student-report-data";
-    if (this.$route.name === "ClassroomStudentPanel") {
-      url = url + "/" + this.$router.currentRoute.params.userId;
-    }
-    this.axios.get(url).then((resp) => {
-      this.assignments_attemps = resp.data.success.assignments_attemps;
-      //   this.assignments_attemps = [];
-      //   console.log(this.assignments_attemps);
-      //   pieChartData.map((data) => {
-      //     this.assignments_attemps.push(data);
-      //   });
-      //   console.log(this.assignments_attemps);
-      //   //   this.getDailyReports(pieChartData);
-      this.showLoader = false;
-    });
-  },
-  methods: {
-    getDailyReports(pieChartData) {
-      //   console.log(pieChartData);
-      pieChartData.map((data) => {
-        this.assignments_attemps = JSON.parse(JSON.stringify(data));
-        console.log(this.assignments_attemps);
-      });
-    },
-  },
+      '/get-student-report-data';
+		if (this.$route.name === 'ClassroomStudentPanel') {
+			url = url + '/' + this.$router.currentRoute.params.userId;
+		}
+		this.axios.get(url).then((resp) => {
+			this.pieChartData = resp.data.success.assignments_attemps;
+			this.dualLineChartData = resp.data.success.average_scores;
+			this.showLoader = false;
+		});
+	},
 };
 </script>
 
