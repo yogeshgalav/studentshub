e704764c9e682c3d82b90b54c79c04523a38231f
jhonnysrinivas <mathasrinivas32@gmail.com>
2021-05-24 05:53:18 +0530

    final

--- app/Http/Controllers/Api/ClassroomUnitController.php
+++ app/Http/Controllers/Api/ClassroomUnitController.php
@@ -34,11 +34,39 @@ class ClassroomUnitController extends Controller
         ->groupBy('units.id')
         ->get();
 
+        $pie_data = DB::table('daily_assignments as da')
+        ->where('da.classroom_id',$request->classroomId)
+        ->select( DB::raw('SUM(CASE WHEN da.status = "activated" THEN 1 ELSE 0 END) AS total_activated'),
+        DB::raw('SUM(CASE WHEN da.status = "draft" THEN 1 ELSE 0 END) AS total_draft'),
+        DB::raw('SUM(CASE WHEN da.status = "completed" THEN 1 ELSE 0 END) AS total_completed'),
+        'da.unit_id')
+        ->groupBy('da.unit_id')
+        ->get();
+
         $bar_data = DB::table('daily_assignments as da')
         ->where('da.classroom_id',$request->classroomId)
         ->leftjoin('daily_reports as dr','dr.daily_assignment_id','=','da.id')
         ->select('da.unit_id',DB::raw('COUNT(distinct dr.user_id) as total_attendes'),DB::raw('AVG(dr.marks_obtained) as average_score'),
         'da.attempt_date')
+        ->groupBy('da.unit_id','da.id','da.attempt_date')
+        ->get();
+
+        $average_summary = DB::table('student_reports as sr')
+        ->leftjoin('units','units.id','=','sr.unit_id')
+        ->where('units.classroom_id',$request->classroomId)
+        ->select('sr.unit_id','sr.score_type',DB::raw('AVG(sr.score) as average_score'))
+        ->groupBy('sr.unit_id','sr.score_type')
+        ->get();
+
+        $table_data = DB::table('daily_assignments as da')
+        ->where('da.classroom_id',$request->classroomId)
+        ->leftjoin('daily_reports as dr','dr.daily_assignment_id','=','da.id')
+        ->leftjoin('daily_questions as dq','dq.daily_assignment_id','=','da.id')
+        ->select('da.id','da.attempt_date',
+                DB::raw('COUNT(distinct dr.user_id) as total_attempt'),
+                DB::raw('AVG(dr.marks_obtained) as average_score'),
+                DB::raw('COUNT(distinct dq.id) as total_questions'),
+                DB::raw('SEC_TO_TIME(AVG(TIME_TO_SEC(dr.duration))) as average_duration'))
         ->groupBy('da.unit_id','da.id','da.attempt_date')
         ->get();
 
@@ -48,7 +76,9 @@ class ClassroomUnitController extends Controller
                 'summary'=>$summary,
                 'daily_assignment_status'=>$daily_assignment_status,
                 'pie_data'=>$pie_data,
-                'bar_data'=>$bar_data
+                'bar_data'=>$bar_data,
+                'table_data'=>$table_data,
+                'average_summary'=>$average_summary
             ]
         ]);
     }
