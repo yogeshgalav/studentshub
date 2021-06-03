3078fdfbeacef4b9678b4ebf9a19a44f1d108ad6
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-06-03 15:29:08 +0530

    Cron job made

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,8 @@
 
 namespace App\Console;
 
-use App\Models\ScheduledJob;
-use Illuminate\Support\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\SthubCron::class,
     ];
 
     /**
@@ -27,15 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        
-        /***
-         * Look through the scheduled jobs table and put the jobs
-         * that are due to dispatched to the queue
-         *
-         * **/
-        $schedule->call(function () {
-            $this->queueScheduledJobs();
-        })->everyMinute()->name('sthub_notifications');
+        $schedule->command('sthub:cron')->everyFiveMinutes();
 
     }
 
@@ -49,28 +38,5 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
-    }
-        /**
-     * @throws Exception
-     */
-    public function queueScheduledJobs()
-    {
-        // Check for all jobs that are due to be run
-        $jobs = ScheduledJob::where('run_at', '<=', Carbon::now('utc'))
-                            ->whereNull('sent_to_queue_at')
-                            ->where('is_completed', '=', 0)
-                            ->get();
-
-        foreach ($jobs as $job) {
-            Log::info('Dispatching scheduled job to queue', [
-                'scheduled_job_id' => $job->id,
-            ]);
-
-            $job_to_queue = new $job->job_type($job);
-            $job_to_queue->dispatch($job);
-
-            $job->sent_to_queue_at = Carbon::now('utc');
-            $job->save();
-        }
     }
 }
