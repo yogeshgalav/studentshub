570ea0be477f1e9ef30d81c3bcc85489b6a2cc6b
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-06-24 10:32:59 +0530

    migration fixed

--- database/migrations/2021_04_21_065558_create_foreign_key_for_classroom_messages_table.php
+++ database/migrations/2021_04_21_065558_create_foreign_key_for_classroom_messages_table.php
@@ -19,7 +19,7 @@ class CreateForeignKeyForClassroomMessagesTable extends Migration
             $table->integer('parent_message_id')->unsigned()->change();
             $table->foreign('classroom_id')->references('id')->on('classrooms')->onDelete('cascade');
             $table->foreign('sender_user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('parent_message_id')->references('id')->on('classroom_messages')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('parent_message_id', 'cm_parent_message_id_foreign')->references('id')->on('classroom_messages')->onDelete('cascade');
         });
     }
 
