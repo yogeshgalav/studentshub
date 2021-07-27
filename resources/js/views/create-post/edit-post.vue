db8d7305b8383544682700563dce37bac7ebfdbb
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-07-27 12:42:02 +0530

    ST-84 post edit page

--- resources/js/views/create-post/edit-post.vue
+++ resources/js/views/create-post/edit-post.vue
@@ -1,10 +1,24 @@
 <template>
-    <div>
-        fffs
-    </div>
+  <div>
+    <create-post-content :new-post="newPost" />
+    <create-post-description :new-post="newPost" />
+  </div>
 </template>
 <script>
+import {mapState} from 'vuex';
+import CreatePostContent from './create-post/create-post-content';
+import CreatePostDescription from './create-post/create-post-description';
+
 export default {
-    props:['post'],
-}
+	components: {
+		CreatePostContent,
+		CreatePostDescription
+	},
+	props:['post'],
+	computed:{
+		...mapState({
+			'newPost': state=>state.new_post,
+		})
+	},
+};
 </script>
