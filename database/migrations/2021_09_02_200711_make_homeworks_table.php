b4b9cbd58254682981ee95068ad31e91cca0aa61
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-09-03 14:31:40 +0530

    ST-103 migration fix

--- database/migrations/2021_09_02_200711_make_homeworks_table.php
+++ database/migrations/2021_09_02_200711_make_homeworks_table.php
@@ -17,12 +17,14 @@ class MakeHomeworksTable extends Migration
             $table->id();
             $table->date('submission_date');
             $table->integer('classroom_id')->unsigned();
+            $table->integer('unit_id')->unsigned();
             $table->integer('teacher_user_id')->unsigned();
             $table->text('description');
             $table->timestamps();
         });
         Schema::table('homeworks', function (Blueprint $table) {
             $table->foreign('classroom_id')->references('id')->on('classrooms')->onDelete('cascade');
+            $table->foreign('unit_id')->references('id')->on('units')->onDelete('cascade');
             $table->foreign('teacher_user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
