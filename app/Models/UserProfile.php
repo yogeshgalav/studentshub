f0129ba971ca05aa777860decd362bc3c1c29f03
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-09-11 02:40:35 +0530

    profile fix

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -9,5 +9,5 @@ class UserProfile extends Model
 {
     use HasFactory;
     protected  $guarded = ['id', 'created_at', 'updated_at'];
-    protected $table ="user_profile";
+    protected $table ="user_profiles";
 }
