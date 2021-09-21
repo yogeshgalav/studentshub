b4a795f43dddcb50ede30b61f5756afed298eba2
Yogesh Galav <mr.yogesh.galav@gmail.com>
2021-09-22 00:52:20 +0530

    custom toolbar

--- resources/js/components/RichTextEditor.vue
+++ resources/js/components/RichTextEditor.vue
@@ -4,6 +4,7 @@
     v-model="content"
     name="homework_html"
     :editor-options="editorSettings"
+    :editor-toolbar="customToolbar"
     :height="'100%'"
   />
 </template>
@@ -28,6 +29,19 @@ export default {
 					imageResize: {},
 				}
 			},
+			customToolbar: [
+				[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+				[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+				[{ 'font': [] }],
+				['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+				[{ 'align': '' }, { 'align': 'center' }, { 'align': 'right' }, { 'align': 'justify' }],
+				['blockquote', 'code-block'],
+				[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+				[{ 'indent': '-1'}, { 'indent': '+1' }],         // outdent/indent
+				[{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+				['link','image','video'],
+				['clean']
+			],
 		};
 	}
 };
