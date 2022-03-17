f724c76e9a1c2a628e93a30cbf78ce5ed555bc13
Anjum <88701810+Anjum7866@users.noreply.github.com>
2022-03-17 04:56:18 -0700

    ask doubt submit button

--- resources/js/Pages/doubt/create-doubt.vue
+++ resources/js/Pages/doubt/create-doubt.vue
@@ -1,51 +1,72 @@
 <template>
   <div>
-    <h1>Anjum</h1>
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Ask Doubt</h1>
+      </div>
+    </div>
     <hr>
-    <form>
-      <div class="">
-        <div class="row">
-          <div
-            class="form-group col-md-6 col-12"
-          >
-            <label for="category">Category</label>
-            <select
-              id="category"
-              v-model="edit_category"
-              name="category"
-              class="form-control"
-            >
-              <option
-                v-for="category in categories"
-                :key="category.id"
-                :value="category.id"
-              >
-                {{ category.name }}
-              </option>
-            </select>
-          </div>
-          <div class="form-group col-md-12">
-            <label for="doubt_question">Question</label>
-            <input
-              id="doubt_question"
-              v-model="doubt_question"
-              class="form-control"
-              type="text"
-              placeholder="Enter Your Question"
-            >
-          </div>
-          <div class="form-group col-md-12">
-            <label for="subject_tags">Subject tags</label>
-            <vue-tags-input
-              v-model="tag"
-              :tags="tags"
-              :autocomplete-items="filteredItems"
-              @tags-changed="newTags => tags = newTags"
-            />
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-body">
+            <form @submit.prevent="addDoubt">
+              <div class="row">
+                <div class="col-md-6 col-12">
+                  <div
+                    class="form-group"
+                  >
+                    <label for="category">Category</label>
+                    <select
+                      id="category"
+                      v-model="edit_category"
+                      name="category"
+                      class="form-control"
+                    >
+                      <option
+                        v-for="category in categories"
+                        :key="category.id"
+                        :value="category.id"
+                      >
+                        {{ category.name }}
+                      </option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="doubt_question">Question</label>
+                    <input
+                      id="doubt_question"
+                      v-model="doubt_question"
+                      class="form-control"
+                      type="text"
+                      placeholder="Enter Your Question"
+                    >
+                  </div>
+                  <div class="">
+                    <label for="subject_tags">Subject tags</label>
+                    <vue-tags-input
+                      v-model="tag"
+                      :tags="tags"
+                      :autocomplete-items="filteredItems"
+                      @tags-changed="newTags => tags = newTags"
+                    />
+                  </div>
+                </div>
+              </div>
+                
+              <div class="mt-2">
+                <button
+                  type="submit"
+                  class="btn btn-primary btn-md"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </form>
+    </div>
   </div>
 </template>
 <script>
@@ -62,7 +83,7 @@ export default {
 			edit_category: 14,
 			tags: [],
 			doubt_question:'',
-            subject_list:[],
+			subject_list:[],
 		};
 	},
 	computed:{
@@ -72,6 +93,24 @@ export default {
 			});
 		},
 	},
-	
+	addDoubt()
+    	{
+    		this.axios.post(this.baseUrl + '/api/add-doubt',{
+    			question:this.doubt_question,
+    			category_id:this.edit_category,
+    			selected_subjects:this.tags,
+    		} )
+    			.then(resp => {
+    				// this.$modal.hide('add_doubt_modal');
+    				this.$refs.addDoubtModal.closeModal();
+    				this.doubt_question='';
+    				this.subject='';
+    				this.getdata();
+    			})
+    			.catch(err => {
+    				reject(err);
+    			});
+    	},
+    	
 };
 </script>
