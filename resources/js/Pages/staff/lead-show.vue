857a44636afff58e1fda5c079589bff6091c3b01
Anjum <88701810+Anjum7866@users.noreply.github.com>
2022-03-21 03:58:58 -0700

    add job page

--- resources/js/Pages/staff/lead-show.vue
+++ resources/js/Pages/staff/lead-show.vue
@@ -1,9 +1,97 @@
 <template>
   <section>
+    <div>
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Anjum Shaikh</h1>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-body">
+              <form>
+                <div class="row">
+                  <div class="col-md-6 col-12">
+                    <div
+                      class="form-group"
+                    >
+                      <label for="category">User Lead Status</label>
+                      
+                       
+                      <select
+                        class="form-control"
+                        aria-label=".form-select-lg example"
+                      >
+                        <option selected>
+                          Lead Status
+                        </option>
+                        <option value="1">
+                          row
+                        </option>
+                        <option value="2">
+                          Invalid
+                        </option>
+                        <option value="3">
+                          Not Intrested
+                        </option>
+                        <option value="4">
+                          Intrested
+                        </option>
+                        <option value="5">
+                          Payment Pending
+                        </option>
+                        <option value="6">
+                          Payment Done
+                        </option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleFormControlTextarea1">Description</label>
+                      <textarea
+                        id="exampleFormControlTextarea1"
+                        class="form-control"
+                        rows="3"
+                      />
+                    </div>
+                 
+                    <div class="form-check">
+                      <input
+                        id="flexCheckDefault"
+                        class="form-check-input"
+                        type="checkbox"
+                        value=""
+                      >
+                      <label
+                        class="form-check-label"
+                        for="flexCheckDefault"
+                      >
+                        User Verified
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="mt-2">
+                  <button
+                    type="submit"
+                    class="btn btn-primary btn-md"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
     <div class="row">
       <div class="col-md-12">
         <accordion
-          title="Details"
+          title="Lead Assign"
           :aria-expanded="true"
         >
           <vue-table-component
@@ -46,17 +134,13 @@ export default {
 			userList:[],
 			userColumns: [
 				{
-					label: 'User Name',
-					field: 'user_name',
-				},
-				{
-					label: 'Phone Number',
-					field: 'phone_no',
+					label: 'Staff Name',
+					field: 'staff_name',
 				},
 				
 				{
-					label: 'Onboarded At',
-					field: 'onboarded_at',
+					label: 'Assigned At',
+					field: 'assigned_at',
 				},
 			]
 		};
--- routes/web/staff.php
+++ routes/web/staff.php
@@ -7,6 +7,7 @@ Route::group(['middleware'=>['auth','admin']],function(){
     Route::get('/lead/{userId}', 'StaffController@leadShowPage');
     Route::get('/manage-courses', 'StaffController@manageCoursePage');
     Route::get('/manage-jobs', 'StaffController@manageJobsPage');
+    Route::get('/add-jobs', 'StaffController@addJobsPage');
     Route::get('/user-feedbacks', 'StaffController@userFeedbacksPage');
     Route::get('/user-reports', 'StaffController@userReportsPage');
 });
