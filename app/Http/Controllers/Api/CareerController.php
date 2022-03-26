e5a286a8c75f58b0f9694e456b32a9d9195f68dc
babinderrathi <102028591+babinderrathi@users.noreply.github.com>
2022-03-26 13:25:03 +0530

    add career page restored

---
+++ app/Http/Controllers/Api/CareerController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use App\Models\Career;
+use DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+
+class CareerController extends Controller
+{
+    public function createOrUpdate (Request $request)
+    {   
+
+        if($request->career_id){
+            $career = Career::find($request->career_id);
+        }
+        else{
+            $career = new Career();
+        }
+
+            $career->name = $request->career_name;
+            $career->category_id = $request->category_id;
+            $career->save();
+
+            return response()->json([
+                'success'=>[
+                    'career'=>$career,
+                ]
+            ]);
+    }
+
+    public function delete ($career_id)
+    {
+        $career = Career::find($career_id);
+        $career->delete();
+        return 'success';
+    }
+} 
--- resources/js/Pages/staff/manage-job.vue
+++ resources/js/Pages/staff/manage-job.vue
@@ -1,40 +1,76 @@
 <template>
   <section>
-    <div class="row">
-      <div class="col-md-12">
-        <accordion
-          title="Details"
-          :aria-expanded="true"
+    <div>
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Manage Career</h1>
+        </div>
+      </div>
+      <hr>
+      <div class="mb-2">
+        <button
+          type="edit"
+          class="btn-lg btn-primary"
+          data-toggle="modal"
+          data-target="#addEditCareerModal"
         >
-          <vue-table-component
-            :columns="userColumns"
-            :rows="userList"
-          >
-            <template
-              slot="table-row"
-              slot-scope="props"
-            >
-              <span v-if="props.column.field==='full_name'">
-                <router-link
-                  :href="'/lead/'+props.row.user_id"
-                  class="text-underline"
-                >{{ props.row['full_name'] }}</router-link>
-              </span>
-            </template>
-            <template slot="emptystate">
-              No user found.
-            </template>
-          </vue-table-component>
-        </accordion>
+          <i class="fas fa-plus" />&nbsp;&nbsp;
+          Add Career
+        </button>
+      </div>
+      <div class="card mb-2 pl-3">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-12">
+              <vue-table-component
+                :columns="careerColumns"
+                :rows="careerList"
+              >
+                <template
+                  slot="table-row"
+                  slot-scope="props"
+                >
+                  <span v-if="props.column.field==='menu'">
+                    <button
+                      class="btn btn-success btn-sm rounded-0"
+                      type="button"
+                      data-placement="top"
+                      title="Edit"
+                      data-toggle="modal"
+                      data-target="#addEditCareerModal"
+                      @click="editCareer(props.row)"
+                    >
+                      <i class="fa fa-edit" />
+                    </button>
+                    <button
+                      class="btn btn-danger btn-sm rounded-0"
+                      type="button"
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      title="Delete"
+                      @click="deleteCareer(props.row)"
+                    >
+                      <i class="fa fa-trash" />
+                    </button>
+                  </span>
+                </template>
+                <template slot="emptystate">
+                  No user found.
+                </template>
+              </vue-table-component> 
+              <!-- Responsive table -->
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-        
+
     <modal
       ref="addEditCareerModal"
       name="addEditCareerModal"
       heading="Add Career"
       classes="modal-md"
-      @submit="addOrEditCareer"
+      @submit="addCareer"
       @cancel="clearModalData"
     >
       <template slot="modalBody">
@@ -47,10 +83,12 @@
               <input
                 id="career_name"
                 v-model="career_name"
+                v-validate="'required'"
                 type="text"
                 class="form-control"
-                name="jname"
+                name="career_name"
               >
+              <span class="error">{{ formErrors('career_name') }}</span>
             </div>
           </div>
           <div
@@ -60,6 +98,7 @@
             <select
               id="category"
               v-model="edit_category"
+              v-validate="'required'"
               name="category"
               class="form-control"
             >
@@ -78,84 +117,89 @@
   </section>
 </template>
 <script>
+import Modal from '../../components/VueNiceModal';
 import VueTableComponent from '@/components/vue-table-component';
-import Accordion from '@/components/accordion.vue';
-
+import FormMixin from '@/components/mixins/form-mixin.js' ;
 import StaffLayout from '@/Layouts/StaffLayout';
-import FormMixin from '@/components/mixins/form-mixin.js';
-
+import careersVue from '../../../../vendor/laravel/horizon/resources/js/screens/metrics';
 export default {
 	layout:StaffLayout,
 	components:{
+    	Modal,
 		VueTableComponent,
-		Accordion
+		
 	},
-	mixins:[FormMixin],
+	mixins: [FormMixin],
 	props:['careers','categories'],
 	data() {
 		return {
-			userList:[],
-			userColumns: [
+			showLoader: false,
+			career_name:'',
+			edit_category:'',
+			career_id:'',
+			careerList:[],
+			careerColumns: [
 				{
-					label: 'User Name',
-					field: 'user_name',
+					label: 'Career Name',
+					field: 'career_name',
 				},
 				{
-					label: 'Phone Number',
-					field: 'phone_no',
+					label: 'Category',
+					field: 'category_name',
 				},
-				
 				{
-					label: 'Onboarded At',
-					field: 'onboarded_at',
+					label: 'Menu',
+					field: 'menu',
 				},
 			]
 		};
 	},
+
 	mounted(){
 		this.careerList=this.careers; 
 	},
 	methods:{
-		addOrEditCareer(){	
+		addCareer(){	
 			this.validateForm().then(valid => {
 				if (valid) {
-					this.careerCreateOrUpdateApi();
+					this.careerCreateOrEditApi();
 				}
 			});
 		},
-		careerCreateOrUpdateApi(){
+		careerCreateOrEditApi(){
 			let loader = this.$loading.show();
-			this.axios.post(this.baseUrl + '/api/career',{
-				career_name:this.career_name,
-				category_id:this.edit_category,
-				career_id:(this.career_id),
-			}).then(resp => {
-				loader.hide();
-				let category_name =this.categories.find(el=>el.id===this.edit_category).name;   			
-				if (this.career_id){
+			  	this.axios.post(this.baseUrl + '/api/career',{
+    			career_name:this.career_name,
+    			category_id:this.edit_category,
+				  career_id:(this.career_id),
+    		})
+    			.then(resp => {
+					loader.hide();
+    				let category_name =this.categories.find(el=>el.id===this.edit_category).name;   			
+					if (this.career_id){
              	let index= this.careerList.findIndex(el=>el.career_id===this.career_id);
 			        this.careerList[index]['career_name']=this.career_name;
 					  	this.careerList[index]['category_id']=this.edit_category;
 					  	this.careerList[index]['category_name']=category_name;
-				}else{
-					this.careerList.push({
-						career_name:resp.data.success.career.name,
-						category_id:resp.data.success.career.category_id,
-						career_id:resp.data.success.career.id,
-						category_name:category_name,
-					});
-				}
-				this.$refs.addEditCareerModal.closeModal();
-				this.clearModalData();
-			});
+					}else{
+						this.careerList.push({
+							career_name:resp.data.success.career.name,
+							category_id:resp.data.success.career.category_id,
+							career_id:resp.data.success.career.id,
+							category_name:category_name,
+						});
+					}
+          	this.$refs.addEditCareerModal.closeModal();
+					this.clearModalData();
+    			});
     	},
 		editCareer(career){
 			this.career_name=career.career_name;
 			this.edit_category=career.category_id;
 			this.career_id=career.career_id;
-		},
-		deleteCareer(career){  	  
-			let loader = this.$loading.show();         
+		    },
+		deleteCareer(career){  	
+			let loader = this.$loading.show();             
 			this.axios.delete('/api/career/'+career.career_id)
 				.then(resp=>{
 					loader.hide();
@@ -163,13 +207,11 @@ export default {
 					this.careerList.splice(index,1);		
 				});
 		},
-		clearModalData(){
-			this.career_name='';
-			this.career_alias='';		
-			this.career_id='';
-			this.edit_category='';
+    	clearModalData(){
+                	this.career_name='';	
+		            this.career_id='';
+		            this.edit_category='';
 		}
-
 	}
 };
 
