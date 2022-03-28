9c92046676ba32b80bbaaee832f9846680fbdb9a
DESKTOP-HG0S5P4\Yogesh Galav <yogesh@groony.onmicrosoft.com>
2022-03-29 00:32:41 +0530

    ST-99 alter category migration and scope query

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,14 @@ class Category extends Model
 {
     protected  $guarded = ['id', 'name', 'slug'];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        statis::addGlobalScope('sortAlpha', function(Buillder $builder) {
+            $builder->whereNull('parent_category_id')->orderBy('name');
+        });
+    }
     public function courses(){
         return $this->hasMany('App\Models\Course');
     }
--- database/migrations/2022_03_25_201832_alter2_categories_table.php
+++ database/migrations/2022_03_25_201832_alter2_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Category;
 
 class Alter2CategoriesTable extends Migration
 {
@@ -18,6 +19,32 @@ class Alter2CategoriesTable extends Migration
             //jobs and course will be added accordingly for better enrichment
             $table->integer('parent_category_id')->unsigned()->nullable();
         });
+        
+        //change law  to law,
+        Category::where('id',10)->update([
+            'name'=>'Law',
+            'slug'=>'law',
+        ]); 
+        Category::where('id',9)->update([
+            'name'=>'Mass Communication',
+            'slug'=>'communication',
+        ]); 
+        //change education to humanities,
+        Category::where('id',7)->update([
+            'name'=>'Humanities',
+            'slug'=>'humanities',
+        ]);
+        //chhange arts&culture to visual and performing then add design to it
+        Category::where('id',4)->update([
+            'name'=>'Performing Arts',
+            'slug'=>'performing-arts',
+        ]);
+        //add design
+        Category::where('id',12)->update([
+            'name'=>'Visual Arts',
+            'slug'=>'visual-arts',
+        ]);
+
     }
 
     /**
