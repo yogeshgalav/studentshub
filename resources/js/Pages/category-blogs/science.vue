36b20a408023f3df3ed9523d7f7e9b9c1ddf4bf7
Anjum <88701810+Anjum7866@users.noreply.github.com>
2022-04-14 03:08:20 -0700

    Show all the hidden data only if AuthUser.is_pro_member is true.

--- resources/js/Pages/category-blogs/science.vue
+++ resources/js/Pages/category-blogs/science.vue
@@ -1,133 +1,141 @@
 <template>
   <div class="row">
-    <div class="col-md-12 mt-1">   
-      <p>Science is the intellectual and practical activity encompassing the systematic study of the structure and behavior of the physical and natural world through observation and experiment. It is a systematic enterprise that builds and organizes knowledge in the form of testable explanations and predictions about the universe Any system of knowledge that is concerned with the physical world and its phenomena and that entails unbiased observations and systematic experimentation. In general, a science involves a pursuit of knowledge covering general truths or the operations of fundamental laws.</p>
-    </div>
     <div class="col-md-12 mt-1">
-      <h4>Quality Required</h4>
+      <article>   
+        <p>Science is the intellectual and practical activity encompassing the systematic study of the structure and behavior of the physical and natural world through observation and experiment. It is a systematic enterprise that builds and organizes knowledge in the form of testable explanations and predictions about the universe Any system of knowledge that is concerned with the physical world and its phenomena and that entails unbiased observations and systematic experimentation. In general, a science involves a pursuit of knowledge covering general truths or the operations of fundamental laws.</p>
+      </article>
     </div>
-    <hr class="mb-0 mt-0">
-    <div class="col-md-12 p-4">
-      <ul class="list-group">
-        <li class>
-          Academic Orientation
-        </li>
-        <li class>
-          Good with Mathematics and Sciences with inclination towards Research
-        </li>
-        <li class>
-          Ability to spend long hours with books and/or computers during study and in the research field
-          during profession.
-        </li>
-      </ul>
-    </div>
-    <div class="col-md-12 mt-1">
-      <h4>Entrance Exams</h4>
-    </div>
-    <hr class="mb-0 mt-0">
-    <div class="col-md-12 p-2">
-      <div class="d-flex pt-2 w-50 justify-content-between">
-        <ul>
-          <h5>You Can be:</h5>
+
+    <div 
+      v-if="AuthUser.is_pro_member"
+      class="row"
+    >
+      <div class="col-md-12 mt-1">
+        <h4>Quality Required</h4>
+      </div>
+      <hr class="mb-0 mt-0">
+      <div class="col-md-12 p-4">
+        <ul class="list-group">
           <li class>
-            KVPY
-          </li>                      
-          <li class>
-            JEE-MAIN
+            Academic Orientation
           </li>
           <li class>
-            JEE-ADVANCED
+            Good with Mathematics and Sciences with inclination towards Research
           </li>
           <li class>
-            NEET
-          </li>
-          <li class>
-            NEST (NISER & DAE)
-          </li>
-          <li class>
-            PAU-CET (Punjab)
-          </li>
-        </ul>
-        <ul>
-          <h5>You Can be:</h5>
-          <li class>
-            BITSAT
-          </li>
-          <li class>
-            NPAT
-          </li>
-          <li class>
-            CMI-EE (Chennai)
-          </li>
-          <li class>
-            ISI-AT (Bangalore)
-          </li>
-          <li class>
-            CUET (Christ)
+            Ability to spend long hours with books and/or computers during study and in the research field
+            during profession.
           </li>
         </ul>
       </div>
-    </div>
-    <div class="col-md-12 mt-1">
-      <h4>Career Options</h4>
-    </div>
-    <hr class="mb-0 mt-0">
-    <div class="col-md-12 p-2">
-      <div class="d-flex pt-2 w-50 justify-content-between">
-        <ul>
-          <h5>You Can be:</h5>
-          <li class>
-            Anthropologist
-          </li>
-          <li class>
-            Bio-chemist
-          </li>
-          <li class>
-            Bio-Physicist
-          </li>
-          <li class>
-            Biotechnologist
-          </li>
-          <li class>
-            Botanist
-          </li>
-          <li class>
-            Chemist
-          </li>
-          <li class>
-            Forensic Scientist
-          </li>
-          <li class>
-            Geologist
-          </li>
-        </ul>
-        <ul>
-          <h5>You Can be:</h5>
-          <li class>
-            Mathematician
-          </li>
-          <li class>
-            Microbiologist
-          </li>
-          <li class>
-            Physical Scientist
-          </li>
-          <li class>
-            Physicist
-          </li>
-          <li class>
-            Research Scientist
-          </li>
-          <li class>
-            Seismologist
-          </li>
-          <li class>
-            Statistician
-          </li>
-          <li class>
-            Zoologist
-          </li>
-        </ul>
+      <div class="col-md-12 mt-1">
+        <h4>Entrance Exams</h4>
+      </div>
+      <hr class="mb-0 mt-0">
+      <div class="col-md-12 p-2">
+        <div class="d-flex pt-2 w-50 justify-content-between">
+          <ul>
+            <h5>You Can be:</h5>
+            <li class>
+              KVPY
+            </li>                      
+            <li class>
+              JEE-MAIN
+            </li>
+            <li class>
+              JEE-ADVANCED
+            </li>
+            <li class>
+              NEET
+            </li>
+            <li class>
+              NEST (NISER & DAE)
+            </li>
+            <li class>
+              PAU-CET (Punjab)
+            </li>
+          </ul>
+          <ul>
+            <h5>You Can be:</h5>
+            <li class>
+              BITSAT
+            </li>
+            <li class>
+              NPAT
+            </li>
+            <li class>
+              CMI-EE (Chennai)
+            </li>
+            <li class>
+              ISI-AT (Bangalore)
+            </li>
+            <li class>
+              CUET (Christ)
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-12 mt-1">
+        <h4>Career Options</h4>
+      </div>
+      <hr class="mb-0 mt-0">
+      <div class="col-md-12 p-2">
+        <div class="d-flex pt-2 w-50 justify-content-between">
+          <ul>
+            <h5>You Can be:</h5>
+            <li class>
+              Anthropologist
+            </li>
+            <li class>
+              Bio-chemist
+            </li>
+            <li class>
+              Bio-Physicist
+            </li>
+            <li class>
+              Biotechnologist
+            </li>
+            <li class>
+              Botanist
+            </li>
+            <li class>
+              Chemist
+            </li>
+            <li class>
+              Forensic Scientist
+            </li>
+            <li class>
+              Geologist
+            </li>
+          </ul>
+          <ul>
+            <h5>You Can be:</h5>
+            <li class>
+              Mathematician
+            </li>
+            <li class>
+              Microbiologist
+            </li>
+            <li class>
+              Physical Scientist
+            </li>
+            <li class>
+              Physicist
+            </li>
+            <li class>
+              Research Scientist
+            </li>
+            <li class>
+              Seismologist
+            </li>
+            <li class>
+              Statistician
+            </li>
+            <li class>
+              Zoologist
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
--- resources/js/Pages/category-blogs/social-studies.vue
+++ resources/js/Pages/category-blogs/social-studies.vue
@@ -1,157 +1,165 @@
 <template>
   <div class="row">
     <div class="col-md-12 mt-1">
-      <p>
-        Human rights are rights inherent to all human beings, regardless of gender, nationality, place of residency, sex, ethnicity, religion, color or and other categorization. Thus, human rights are non-discriminatory, meaning that all human beings are entitled to them and cannot be excluded from them. Of course, while all human beings are entitled to human rights, not all human beings experience them equally throughout the world. Many governments and individuals ignore human rights and grossly exploit other human beings.
+      <article>
+        <p>
+          Human rights are rights inherent to all human beings, regardless of gender, nationality, place of residency, sex, ethnicity, religion, color or and other categorization. Thus, human rights are non-discriminatory, meaning that all human beings are entitled to them and cannot be excluded from them. Of course, while all human beings are entitled to human rights, not all human beings experience them equally throughout the world. Many governments and individuals ignore human rights and grossly exploit other human beings.
 
-        There are a variety of human rights, including:
-        Civil rights (such as the rights to life, liberty and security),
-        Political rights (like rights to the protection of the law and equality before the law),
-        Economic rights (including rights to work, to own property and to receive equal pay),
-        Social rights (like rights to education and consenting marriages),
-        Cultural rights (including the right to freely participate in their cultural community), and
-        Collective rights (like the right Career in Law and Humanity
-      </p>
+          There are a variety of human rights, including:
+          Civil rights (such as the rights to life, liberty and security),
+          Political rights (like rights to the protection of the law and equality before the law),
+          Economic rights (including rights to work, to own property and to receive equal pay),
+          Social rights (like rights to education and consenting marriages),
+          Cultural rights (including the right to freely participate in their cultural community), and
+          Collective rights (like the right Career in Law and Humanity
+        </p>
+      </article>
     </div>
-    <div class="col-md-12 mt-1">
-      <h4>Quality Required</h4>
-    </div>
-    <hr class="mb-0 mt-0">
-    <div class="col-md-12 p-2">
-      <ul>
-        <li class>
-          Academic, People and Creative Orientation
-        </li>
-        <li class>
-          Good with Logical Thinking and Arguments Interest in Politics and Social Issues
-        </li>
-        <li class>
-          Ability to spend long hours in reading and writing both during study and later in the profession.
-        </li>
-      </ul>
-    </div>
-    <div class="col-md-12 mt-1">
-      <h4>Entrance Exams</h4>
-    </div>
-    <hr class="mb-0 mt-0">
-    <div class="col-md-12 p-2">
-      <div class="d-flex pt-2 w-50 justify-content-between">
+
+    <div 
+      v-if="AuthUser.is_pro_member"
+      class="row"
+    >
+      <div class="col-md-12 mt-1">
+        <h4>Quality Required</h4>
+      </div>
+      <hr class="mb-0 mt-0">
+      <div class="col-md-12 p-2">
         <ul>
-          <h5>You Can be:</h5>
           <li class>
-            CLAT (21 NLU’s)
+            Academic, People and Creative Orientation
           </li>
           <li class>
-            AILET (NLU-Delhi)
+            Good with Logical Thinking and Arguments Interest in Politics and Social Issues
           </li>
           <li class>
-            SLAT (Symbiosis)
-          </li>
-          <li class>
-            CUET (Christ)
-          </li>
-          <li class>
-            BLAT (BHU)
-          </li>
-          <li class>
-            CET-BVP
-          </li>
-          <li class>
-            JMI-ET (Jamia)
-          </li>
-          <li class>
-            AMU-ET (Aligarh)
-          </li>
-        </ul>
-        <ul>
-          <h5>You Can be:</h5>
-          <li class>
-            MH-CET
-          </li>
-          <li class>
-            ULSAT (UPES)
-          </li>
-          <li class>
-            KIIT-ET (Kalinga)
-          </li>
-          <li class>
-            MSU-ET
-          </li>
-          <li class>
-            AIL-LET (Army Inst.)
-          </li>
-          <li class>
-            KLEE (Kerala)
-          </li>
-          <li class>
-            RULET (Rajasthan)
-          </li>
-          <li class>
-            PU-LLB-ET
+            Ability to spend long hours in reading and writing both during study and later in the profession.
           </li>
         </ul>
       </div>
-    </div>
-    <div class="col-md-12 mt-1">
-      <h4>Career Options</h4>
-    </div>
-    <hr class="mb-0 mt-0">
-    <div class="col-md-12 p-2">
-      <div class="d-flex pt-2 w-50 justify-content-between">
-        <ul>
-          <h5>You Can be:</h5>
-          <li class>
-            Advocate
-          </li>
-          <li class>
-            Arbitrator
-          </li>
-          <li class>
-            Conciliator
-          </li>
-          <li class>
-            Corporate Legal Consultant
-          </li>
-          <li class>
-            Diplomat
-          </li>
-          <li class>
-            Investigator
-          </li>
-          <li class>
-            Judge
-          </li>
-          <li class>
-            Judicial Magistrate
-          </li>
-        </ul>
-        <ul>
-          <h5>You Can be:</h5>
-          <li class>
-            Lawyer
-          </li>
-          <li class>
-            Legal Advisor
-          </li>
-          <li class>
-            Litigator
-          </li>
-          <li class>
-            Mediator
-          </li>
-          <li class>
-            Professor
-          </li>
-          <li class>
-            Prosecutor
-          </li>
-          <li class>
-            Social Activist
-          </li>
-          <li class>
-            Solicitor
-          </li>
-        </ul>
+      <div class="col-md-12 mt-1">
+        <h4>Entrance Exams</h4>
+      </div>
+      <hr class="mb-0 mt-0">
+      <div class="col-md-12 p-2">
+        <div class="d-flex pt-2 w-50 justify-content-between">
+          <ul>
+            <h5>You Can be:</h5>
+            <li class>
+              CLAT (21 NLU’s)
+            </li>
+            <li class>
+              AILET (NLU-Delhi)
+            </li>
+            <li class>
+              SLAT (Symbiosis)
+            </li>
+            <li class>
+              CUET (Christ)
+            </li>
+            <li class>
+              BLAT (BHU)
+            </li>
+            <li class>
+              CET-BVP
+            </li>
+            <li class>
+              JMI-ET (Jamia)
+            </li>
+            <li class>
+              AMU-ET (Aligarh)
+            </li>
+          </ul>
+          <ul>
+            <h5>You Can be:</h5>
+            <li class>
+              MH-CET
+            </li>
+            <li class>
+              ULSAT (UPES)
+            </li>
+            <li class>
+              KIIT-ET (Kalinga)
+            </li>
+            <li class>
+              MSU-ET
+            </li>
+            <li class>
+              AIL-LET (Army Inst.)
+            </li>
+            <li class>
+              KLEE (Kerala)
+            </li>
+            <li class>
+              RULET (Rajasthan)
+            </li>
+            <li class>
+              PU-LLB-ET
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-12 mt-1">
+        <h4>Career Options</h4>
+      </div>
+      <hr class="mb-0 mt-0">
+      <div class="col-md-12 p-2">
+        <div class="d-flex pt-2 w-50 justify-content-between">
+          <ul>
+            <h5>You Can be:</h5>
+            <li class>
+              Advocate
+            </li>
+            <li class>
+              Arbitrator
+            </li>
+            <li class>
+              Conciliator
+            </li>
+            <li class>
+              Corporate Legal Consultant
+            </li>
+            <li class>
+              Diplomat
+            </li>
+            <li class>
+              Investigator
+            </li>
+            <li class>
+              Judge
+            </li>
+            <li class>
+              Judicial Magistrate
+            </li>
+          </ul>
+          <ul>
+            <h5>You Can be:</h5>
+            <li class>
+              Lawyer
+            </li>
+            <li class>
+              Legal Advisor
+            </li>
+            <li class>
+              Litigator
+            </li>
+            <li class>
+              Mediator
+            </li>
+            <li class>
+              Professor
+            </li>
+            <li class>
+              Prosecutor
+            </li>
+            <li class>
+              Social Activist
+            </li>
+            <li class>
+              Solicitor
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
