84e4b4b6530e46b1fb283fcfc587bbd9514e0258
Anjum <88701810+Anjum7866@users.noreply.github.com>
2022-05-12 02:23:23 -0700

    profile social icon working perfectly

--- resources/js/Pages/profile/profile.vue
+++ resources/js/Pages/profile/profile.vue
@@ -68,7 +68,7 @@
                         target="_blank"
                         :href="user.insta_url ? ('https://instagram.com/'+user.insta_url) :'#'"
                         :disabled="user.insta_url ? false:true"
-                        class="icoInstagram disabled"
+                        :class="['icoInstagram', user.insta_url ? '' :'disabled']"
                         title="Instagram"
                       ><i class="fab fa-instagram" /></a>
                     </li>
@@ -77,7 +77,7 @@
                         target="_blank"
                         :href="user.fb_url ? user.fb_url :'#'"
                         :disabled="user.fb_url ? false:true"
-                        class="icoFacebook disabled"
+                        :class="['icoFacebook', user.fb_url ? '' :'disabled']"
                         title="Facebook"
                       ><i class="fab fa-facebook-f" /></a>
                     </li>
@@ -87,6 +87,7 @@
                         :disabled="user.linkedin_url ? false:true"
                         target="_blank"
                         class="icoLinkedin disabled"
+                        :class="['icoLinkedin', user.linkedin_url ? '' :'disabled']"
                         title="Linkedin"
                       ><i class="fab fa-linkedin" /></a>
                     </li>
@@ -95,7 +96,7 @@
                         target="_blank"
                         :href="user.email ? 'mailto:'+user.email :'#'"
                         :disabled="user.email ? false:true"
-                        class="icoInstagram disabled"
+                        :class="['icoInstagram', user.email ? '' :'disabled']"
                         title="Email"
                       ><i class="fa fa-envelope" /></a>
                     </li>
