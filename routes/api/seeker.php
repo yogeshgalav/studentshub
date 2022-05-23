96e1d68bf5d23ebe5847d6177be71d81c095cfe5
Anjum <88701810+Anjum7866@users.noreply.github.com>
2022-05-23 03:31:37 -0700

    account setting education details and teaching detail added

--- routes/api/seeker.php
+++ routes/api/seeker.php
@@ -17,6 +17,8 @@ Route::group(['middleware' => ['auth:api']], function () {
         //profile
     Route::get('/get-profile','UserController@getProfile');
     Route::post('/save-profile', 'UserController@saveProfile');
+    Route::delete('/delete-teachersdetails/{teacher}','UserController@deleteTeacherDetails');
+    Route::delete('/delete-studentdetails/{student}','UserController@deleteStudentDetails');
     Route::post('/checkin/student', [App\Http\Controllers\Api\StudentController::class, 'create']);
     Route::post('/checkin/teacher', 'InstituteUserController@teacherCheckin');
     Route::get('/search-user', [App\Http\Controllers\Api\SearchController::class, 'searchUser']);
