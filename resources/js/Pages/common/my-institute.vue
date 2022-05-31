144b893df72ff156caae1b912c1bad72a9534179
Anjum <88701810+Anjum7866@users.noreply.github.com>
2022-05-31 00:02:52 -0700

    my-institute static page created

--- resources/js/Pages/common/my-institute.vue
+++ resources/js/Pages/common/my-institute.vue
@@ -3,10 +3,67 @@
     <Head>
       <title>{{ institute ? institute.name : 'My Institute' }}</title>
     </Head>
-    <div class="col-md-12 mt-3">
-      <h1>{{ institute ? institute.name : 'My Institute' }}</h1>
-    </div>
-    <hr>
+    <!-- Header -->
+   
+    <header
+      id="home"
+      style="max-width:1200px;min-width:400px"
+    >
+      <div class="card">
+        <div class="card-body">
+          <img
+            src="/images/institute.jpg"
+            width="1000"
+            height="450"
+            style="margin-bottom: -50px;"
+          >
+          <router-link
+            href="/"
+          >
+            <img
+              src="/logo.png"
+              alt="Student Hub"
+              width="100"
+              height="100"
+              style=" margin-left:10px;"
+            >
+          </router-link>
+          <h3>
+            {{ institute ? institute.name : 'My Institute' }}
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+            Aenean a urna non felis elementum varius.
+          </p>
+          <div class="social-network_singlepage">
+            <social-sharing
+              :url="
+                AuthUser.full_name + ' has invited you to join '+ institute.name+' on Students Hub. click the link below to join now \n '+ baseUrl + '/get-started?inId=' + institute.id
+              "
+              inline-template
+            >
+              <div class="post_content_social">
+                <network network="facebook">
+                  <p class="post_content_social_icon">
+                    <i class="fab fa-facebook-f" />
+                  </p>
+                </network>
+                <network network="twitter">
+                  <p><i class="fab fa-twitter" /></p>
+                </network>
+                <network network="reddit">
+                  <p><i class="fab fa-reddit" /></p>
+                </network>
+                <network network="email">
+                  <p><i class="fa fa-envelope" /></p>
+                </network>
+              </div>
+            </social-sharing>
+          </div>
+        </div>
+      </div>
+    </header>
+    
     <div
       v-if="!AuthUser.preferred_institute_id"
       class="col-md-12"
@@ -32,7 +89,7 @@
     </div>
     <div
       v-if="AuthUser.preferred_institute_id"
-      class="col-md-12"
+      class="col-md-12 mt-3"
     >
       <nav-tabs
         :tabs="tabs"
@@ -237,7 +294,6 @@
     </div>
   </div>
 </template>
-<style scoped></style>
 <script>
 import NavTabs from '../../components/NavTabs';
 import SelectInstitute from '../../components/SelectInstitute.vue';
