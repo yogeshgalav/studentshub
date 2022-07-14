e3b6104401ccae65ce8a7ac45e4463ab8ca0ee1c
yogesh galav <yogesh@groonyindia.in>
2022-07-14 07:46:31 +0530

    Version-2.1 boot.sh

--- boot.sh
+++ boot.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 composer install
-npm i
-php artisan migrate:fresh
-php artisan db:seed
-php artisan passport:install
-php artisan passport:client --personal --name="Actionable Conversations Personal Access Client"
+npm install
+php artisan cache:clear
+php artisan migrate
 npm run watch
