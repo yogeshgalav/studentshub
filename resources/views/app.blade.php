e27fbdc46ca75bc8b2f8cb70bb6b73df9393a0e6
Yogesh Galav <mr.yogesh.galav@gmail.com>
2022-09-01 06:36:29 +0530

    hotjar and design fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,7 @@
     <script>
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:2980989,hjsv:6};
+            h._hjSettings={hjid:2171982,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
