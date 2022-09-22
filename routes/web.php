f0161362c5b6afdd45635a58be7711988c3c8a74
i-am-riya <mriya99s@yahoo.com>
2022-09-22 15:47:46 +0530

    Post Card And Show Route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use Inertia\Inertia;
 
 /*
@@ -21,6 +22,7 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('/');
+Route::get('/post/{id}', [PostController::class, 'show']);
 
 Route::get('home', [HomeController::class, 'index'])->name('home');
 Route::resource('employee', EmployeeController::class)->only(['index', 'store', 'update', 'destroy']);
