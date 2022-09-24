4e1b6de75c1fa96970fc1ac45568e2c4804e1524
yogeshGroony <yogesh@groonyindia.in>
2022-09-25 02:23:30 +0530

    Vite and tailwind configured
    webpack and vuetify removed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
-    <link href="{{ mix('/js/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
-    @routes
-  </head>
-  <body>
-    @inertia
-  </body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Laravel with Inertia</title>
+		@vite
+	</head>
+	<body class="antialiased bg-gray-900">
+		@inertia
+	</body>
 </html>
