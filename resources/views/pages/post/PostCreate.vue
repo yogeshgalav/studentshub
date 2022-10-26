ed7cee15340f25e74357dfc24270169b1c5d8443
er-ashu-sharma <112608038+er-ashu-sharma@users.noreply.github.com>
2022-10-26 17:28:57 +0530

    post create markdown it implement

--- resources/views/pages/post/PostCreate.vue
+++ resources/views/pages/post/PostCreate.vue
@@ -1,6 +1,7 @@
 
 <template>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css"/>
 	<div class="container mt-5">
     <div class="d-flex justify-content-center align-items-center">
         <div class="col-lg-5 card w-96 p-5">
@@ -17,12 +18,19 @@
                 </div>
                 <div class="mb-3 form-group">
 					<label class="mb-1"> {{ 'Content' }} </label>
-					<input
+					<textarea
                         v-model="content"
-                        type="text"
                         class="form-control"
                         name="content"
+                        v-on:keyup="onChange"
+                        rows="10"
+                        cols="10"
                       >
+                      </textarea>
+                </div>
+                <label class="mb-1"> {{ 'Result' }} </label>
+                <div id="result" style="height:200px; width:320px; background-color: skyblue;">
+                    {{ result }}
                 </div>
                 <div class="mb-3 form-group">
                     <label class="mb-1"> {{ 'Classroom Id' }} </label>
@@ -49,7 +57,14 @@
 </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent , ref } from 'vue';
+import axios from 'axios';
+import MarkdownIt from 'markdown-it';
+import MarkdownItSub from 'markdown-it-sub';
+import MarkdownItSup from 'markdown-it-sup';
+import MarkdownItKatex from 'markdown-it-katex';
+import 'markdown-it-latex/dist/index.css';
+// import MarkdownItLatex from 'markdown-it-latex';
 
 export default defineComponent({
     setup() {
@@ -61,11 +76,31 @@ export default defineComponent({
 			content: '',
 			subject_id: '',
             primary_image_url: '',
+            result:'',
 		};
 	},
 	mounted(){
-		
+
 	},methods:{
+
+        onChange(event){
+            const DEFAULT_OPTIONS_LINK_ATTRIBUTES = {
+        attrs: {
+            target: '_blank',
+            rel: 'noopener'
+            }
+        };
+        const DEFAULT_OPTIONS_KATEX = { throwOnError: false, errorColor: '#cc0000' }
+
+        const options =  {markdownIt: {
+            linkify: true
+          },
+          katex: DEFAULT_OPTIONS_KATEX,
+          linkAttributes: DEFAULT_OPTIONS_LINK_ATTRIBUTES,}
+            let md1 = new MarkdownIt(options).use(MarkdownItSub).use(MarkdownItSup).use(MarkdownItKatex);
+            console.log(md1);
+            document.getElementById("result").innerHTML = md1.render(this.content);
+        },
 		postCreate() {
 
 					this.axios.post('/api/post/create', {
